e0c1c927ab4507b7fcb6e0d7963ca34547dc03ad
lokesh <lokeshbadisa657@gmail.com>
2023-05-20 12:31:32 +0530

    Add Social Media Logins

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
+
 // import { BrowserRouter} from 'react-router-dom';
 // import { Route,Routes} from 'react-router-dom';
-import NavBar from './Components/NavBar';
+// import NavBar from './Components/NavBar';
 import Login from './Components/login';
 
 //Components should always be first letter capital. Very strange thing!!
 
 function App() {
-
-
+     
 
   return (
     <>
-    <NavBar/>
-    <div className="container" style={{}}>
+    {/* <NavBar/> */}
+    {/* <div className="container" style={{}}>
       <Login/>
-    </div>
+    </div> */}
     </>
+
   );
 }
 
