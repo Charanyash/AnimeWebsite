0aff3780cf22bae00275442666efa2dd46610a24
Aakash Kamuju <kamujuaakash@gmail.com>
2023-05-22 19:18:23 +0530

    modified app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import NavBar from './Components/NavBar';
+import Login from './Components/login'
 import Signup from './Components/Signup';
 function App() {
   return (
