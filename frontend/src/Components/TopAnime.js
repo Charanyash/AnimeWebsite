3e3e8f781374a852d5af01cb6fdcdc6473e09632
Aakash Kamuju <kamujuaakash@gmail.com>
2023-05-22 18:59:36 +0530

    added topanime page

---
+++ frontend/src/Components/TopAnime.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Cards from "./Cards";
+const cardlist = [{title:"Gintam" ,url:"https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg",description:"kaizoku ou ni orewa nar" }]
+
+function TopAnime(){
+    <>
+    <Cards cardlist = {cardlist}/>
+    </>
+}
