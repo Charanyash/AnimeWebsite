3e1fefbaccd2aa103bc6a08540a71c2b5a3a0f49
Aakash Kamuju <kamujuaakash@gmail.com>
2023-05-30 09:57:13 +0530

    minor changes

--- frontend/src/Components/TopAnime.js
+++ frontend/src/Components/TopAnime.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useState} from "react";
 import NavBar from "./NavBar";
 import Genrebar from './Genrebar'
@@ -41,24 +40,8 @@ function TopAnime(){
         </div>
     </form>
     {/* <Cards cardlist = {cardlist}/> */}
-=======
-import React from "react";
-import Cards from "./Cards";
-import NavBar from "./NavBar";
-const cardlist = [{title:"Black Clover" ,url:"https://img.zorores.com/_r/300x400/100/f5/8b/f58b0204c20ae3310f65ae7b8cb9987e/f58b0204c20ae3310f65ae7b8cb9987e.jpg",description:"genkai wa koero" },{title:"Black Clover" ,url:"https://img.zorores.com/_r/300x400/100/f5/8b/f58b0204c20ae3310f65ae7b8cb9987e/f58b0204c20ae3310f65ae7b8cb9987e.jpg",description:"genkai wa koero" },{title:"Black Clover" ,url:"https://img.zorores.com/_r/300x400/100/f5/8b/f58b0204c20ae3310f65ae7b8cb9987e/f58b0204c20ae3310f65ae7b8cb9987e.jpg",description:"genkai wa koero" },{title:"Black Clover" ,url:"https://img.zorores.com/_r/300x400/100/f5/8b/f58b0204c20ae3310f65ae7b8cb9987e/f58b0204c20ae3310f65ae7b8cb9987e.jpg",description:"genkai wa koero" },{title:"Black Clover" ,url:"https://img.zorores.com/_r/300x400/100/f5/8b/f58b0204c20ae3310f65ae7b8cb9987e/f58b0204c20ae3310f65ae7b8cb9987e.jpg",description:"genkai wa koero" },{title:"Black Clover" ,url:"https://img.zorores.com/_r/300x400/100/f5/8b/f58b0204c20ae3310f65ae7b8cb9987e/f58b0204c20ae3310f65ae7b8cb9987e.jpg",description:"genkai wa koero" },{title:"Black Clover" ,url:"https://img.zorores.com/_r/300x400/100/f5/8b/f58b0204c20ae3310f65ae7b8cb9987e/f58b0204c20ae3310f65ae7b8cb9987e.jpg",description:"genkai wa koero" },{title:"Black Clover" ,url:"https://img.zorores.com/_r/300x400/100/f5/8b/f58b0204c20ae3310f65ae7b8cb9987e/f58b0204c20ae3310f65ae7b8cb9987e.jpg",description:"genkai wa koero" },]
-
-function TopAnime(){
-    return(
-    <>
-    <NavBar/>
-    <Cards cardlist = {cardlist}/>
->>>>>>> main
     </>
     );
 }
 
-<<<<<<< HEAD
 export default TopAnime;
-=======
-export default TopAnime;
->>>>>>> main
