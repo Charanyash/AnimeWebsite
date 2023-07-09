7240d5fc4e4ebb37f0abc09b627f36ccab229cd8
Aakash Kamuju <kamujuaakash@gmail.com>
2023-07-09 20:41:19 +0530

    modified cards.js

--- frontend/src/Components/Cards.js
+++ frontend/src/Components/Cards.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React,{useState} from 'react';
+import { Link } from 'react-router-dom';
+import './Cards.css'
+
 
 function Cards(props) {
+  const [page, setPage] = useState(1);
+  const perPage = 6;
+  const startIndex = (page - 1) * perPage;
+  const endIndex = startIndex + perPage;
+  const paginatedCards = props.cardlist.slice(startIndex, endIndex);
     return (
         <>        
-        <div className="row row-cols-1 row-cols-md-5 g-2">
-      {props.cardlist.map((item,index) => (<div className="col" key={index}>
-            <div className="card" style={{ width: '18rem', height: '100%' }}>
+        <div className="row row-cols-1 row-cols-md-6 g-2">
+      {paginatedCards.map((item,index) => (<div className="col" key={index}>
+            <div className="card" style={{ width: '12rem', height: '100%' }}>
+            <Link className='link' to={`/anime/${item.title}`}>
               <img src = {item.url} className="card-img-top" alt="https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg" />
               <div className="card-body">
+                
                 <h5 className="card-title">{item.title}</h5>
-                <p className="card-text">
+                {/* <p className="card-text">
                   {item.description}
-                </p>
+                </p> */}
               </div>
+              </Link>
             </div>
           </div>))}
-        </div>
+          </div>
+          <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+            Previous
+          </button>
+          <button onClick={() => setPage(page + 1)} disabled={endIndex >= props.cardlist.length}>
+            Next
+          </button>
+        
         </>
         );
   }
--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import NavBar from "./NavBar";
 import Cards from "./Cards";
-const cardlist2 = [{tilte:"Demon Slayer", url:"https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",description:"oni nare ..................................................... -akaza"},{tilte:"Demon Slayer", url:"https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",description:"oni nare ..................................................... -akaza"},{tilte:"Demon Slayer", url:"https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",description:"oni nare ..................................................... -akaza"},{tilte:"Demon Slayer", url:"https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",description:"oni nare ..................................................... -akaza"},{tilte:"Demon Slayer", url:"https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",description:"oni nare ..................................................... -akaza"},{tilte:"Demon Slayer", url:"https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",description:"oni nare ..................................................... -akaza"}]
-const cardlist = [{title:"Gintam" ,url:"https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg",description:"kaizoku ou ni orewa nar" },{title:"Gintam" ,url:"https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg",description:"kaizoku ou ni orewa nar" },{title:"Gintam" ,url:"https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg",description:"kaizoku ou ni orewa nar" },{title:"Gintam" ,url:"https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg",description:"kaizoku ou ni orewa nar" },{title:"Gintam" ,url:"https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg",description:"kaizoku ou ni orewa nar" },{title:"Gintam" ,url:"https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg",description:"kaizoku ou ni orewa nar" },{title:"Gintam" ,url:"https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg",description:"kaizoku ou ni orewa nar" },{title:"Gintam" ,url:"https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg",description:"kaizoku ou ni orewa nar" }]
+import { useParams } from "react-router-dom";
+const cardlist2 = [{title:"DemonSlayer", url:"https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",description:"oni nare ..........................................jjjjjjjjjjjjjjjjjjjjjjj.......... -akaza"},{title:"Demon Slayer", url:"https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",description:"oni nare ..................................................... -akaza"},{title:"Demon Slayer", url:"https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",description:"oni nare ..................................................... -akaza"},{title:"Demon Slayer", url:"https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",description:"oni nare ..................................................... -akaza"},{title:"Demon Slayer", url:"https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",description:"oni nare ..................................................... -akaza"},{title:"Demon Slayer", url:"https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",description:"oni nare ..................................................... -akaza"}]
+const cardlist = [{title:"Gintama" ,url:"https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg",description:"kaizoku ou ni orewa nar" },{title:"Gintam" ,url:"https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg",description:"kaizoku ou ni orewa nar" },{title:"Gintam" ,url:"https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg",description:"kaizoku ou ni orewa nar" },{title:"Gintam" ,url:"https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg",description:"kaizoku ou ni orewa nar" },{title:"Gintam" ,url:"https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg",description:"kaizoku ou ni orewa nar" },{title:"Gintam" ,url:"https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg",description:"kaizoku ou ni orewa nar" },{title:"Gintam" ,url:"https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg",description:"kaizoku ou ni orewa nar" },{title:"Gintam" ,url:"https://img.zorores.com/_r/300x400/100/5a/2f/5a2f7d0736f223f096c459751f6f0bd6/5a2f7d0736f223f096c459751f6f0bd6.jpg",description:"kaizoku ou ni orewa nar" }]
+
+
 
 function Home(){
+    const {id} = useParams();
+    console.log(id);
+
+    const [data,setData] = useState([]);
+    const [data2,setData2] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+      }, []);
+    
+      useEffect(() => {
+        fetchData2();
+      }, []);
+
+      const fetchData = async() => {
+        try {
+            const response = await fetch('your-api-endpoint');
+            const jsonData = await response.json();
+            setData(jsonData);
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        };
+    
+    const fetchData2 = async() => {
+        try {
+            const response = await fetch('your-api-endpoint2');
+            const jsonData = await response.json();
+            setData2(jsonData);
+            } catch (error) {
+            console.error('Error fetching data:', error);
+            }
+        };
+
     return (
     <>
     <NavBar/>
